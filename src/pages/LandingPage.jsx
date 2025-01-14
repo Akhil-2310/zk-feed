ae16c650e6b23c3cfb950cf778ceb81a9712a24b
Akhil-2310 <akhilnanavati23@gmail.com>
2025-01-14 16:05:50 +0530

    landing changes

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function LandingPage() {
   return (
     <div className="flex flex-col min-h-screen">
@@ -7,29 +9,21 @@ function LandingPage() {
             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
               <span className="block">Welcome to ZK Feed</span>
               <span className="block text-indigo-600">
-                Anonymous Feedback with Zero-Knowledge Proofs
+                Anonymous Signals with Zero-Knowledge Proofs
               </span>
             </h1>
             <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-              Share your thoughts anonymously and securely using cutting-edge
+              Share your thoughts/feedback/votes anonymously and securely using cutting-edge
               zero-knowledge technology.
             </p>
             <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
               <div className="rounded-md shadow">
-                <a
-                  href="#"
+                <Link
+                  to="/generate-identity"
                   className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
                 >
                   Get started
-                </a>
-              </div>
-              <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
-                <a
-                  href="#"
-                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
-                >
-                  Learn more
-                </a>
+                </Link>
               </div>
             </div>
           </div>
