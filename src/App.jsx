dc05414a235a55d596877faf0f241621ea6cc0e9
Akhil-2310 <akhilnanavati23@gmail.com>
2025-01-14 16:30:45 +0530

    routes and packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from "./components/Navbar";
+import LandingPage from "./pages/LandingPage";
+import GenerateIdentity from "./pages/GenerateIdentity";
+import CreateGroup from "./pages/CreateGroup";
+import AllGroups from "./pages/AllGroups";
+import ViewGroup from "./pages/ViewGroup";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="min-h-screen bg-gray-100">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/generate-identity" element={<GenerateIdentity />} />
+          <Route path="/create-group" element={<CreateGroup />} />
+          <Route path="/all-groups" element={<AllGroups />} />
+          <Route path="/view-group/:id" element={<ViewGroup />} />
+        </Routes>
+        <ToastContainer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
